d8e74967c0687600f2c8c0d2b71917115eb05241
Policki <enej31@gmail.com>
2025-10-23 11:09:52 +0200

    naredil class Kvader

--- Program.cs
+++ Program.cs
@@ -12,8 +12,20 @@ namespace vaja_6_2
         static void Main(string[] args)
         {
             Pit_Trikotnik t1 = new Pit_Trikotnik(6, 8);
-
             Console.WriteLine(t1.Ploscina());
+
+
+            Kvader k1 = new Kvader(3, 3);
+            Kvader k2 = new Kvader(6, 4);
+
+            Console.WriteLine("Kvader 1");
+            k1.VrniObseg();
+            k1.VrniPloscino();
+            Console.WriteLine("");
+            Console.WriteLine("Kvader 2");
+            k2.VrniObseg();
+            k2.VrniPloscino();
+
         }
     }
 }
